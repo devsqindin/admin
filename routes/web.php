86bd498ff24f7ce5cf339a0d1896f0a1f83daf50
Thiago Bello <thiagobello@MacBook-Air-de-Thiago.local>
2021-10-03 10:24:03 -0300

    dev

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return redirect('/admin');
-});
+// Route::get('/', function () {
+//     return redirect('/admin');
+// });
 
 /*Route::get('/datetime', function () {
 	echo date("d/m/Y H:i:s");
 });*/
+
+Route::get('/','AdminController@login')->name('login');
 
 Route::prefix('admin')->name('admin.')->group(function () {
 	Route::get('/','AdminController@login')->name('login');
