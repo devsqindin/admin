5229ac9bc091b1db84d9b41f05d39ad1c7e09052
devwdougherty <devwdougherty@outlook.com>
2021-10-06 12:02:46 -0300

    ações de troca e reset de email, mudadas de api desbankei para andromeda prod

--- resources/views/desbankei/new_email.blade.php
+++ resources/views/desbankei/new_email.blade.php
@@ -14,7 +14,7 @@ Olá, {{$user->nome_completo}}!
 <!-- BOTÃO -->
 <tr>
   <td align="center" style="padding:0px 0px 16px 0px">
-    <a href="https://api.desbankei.com.br/reset-email?token={{$user->reset_email}}" style="font-family:'Source Sans Pro','Helvetica Neue',Calibre,Helvetica,Arial,sans-serif;font-weight:400;font-size:14px;line-height:24px;color:#1a33ec;text-decoration:none;background-color:#ffffff;border-top:1px solid #1a33ec;border-bottom:1px solid #1a33ec;border-right:1px solid #1a33ec;border-left:1px solid #1a33ec;border-radius:4px;display:inline-block;padding:20px;" target="_blank">ALTERAR E-MAIL</a>
+    <a href="https://andromeda-prod10397524v3.qindin.com.br/reset-email?token={{$user->reset_email}}" style="font-family:'Source Sans Pro','Helvetica Neue',Calibre,Helvetica,Arial,sans-serif;font-weight:400;font-size:14px;line-height:24px;color:#1a33ec;text-decoration:none;background-color:#ffffff;border-top:1px solid #1a33ec;border-bottom:1px solid #1a33ec;border-right:1px solid #1a33ec;border-left:1px solid #1a33ec;border-radius:4px;display:inline-block;padding:20px;" target="_blank">ALTERAR E-MAIL</a>
   </td>
 </tr>
 <!-- END BOTÃO -->
