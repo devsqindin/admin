670b389745d99175efa3c9d8d36d457d75c305eb
Infra Qindin <infra@qindin.com.br>
2021-10-28 16:38:48 +0000

    De dentro do server: classes vendor para arrumar o deploy automatico

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,15 +22,13 @@ class ComposerAutoloaderInite8bd5a886ab815494e9f717f8d708d92
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInite8bd5a886ab815494e9f717f8d708d92', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInite8bd5a886ab815494e9f717f8d708d92', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require __DIR__ . '/autoload_static.php';
+            require_once __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInite8bd5a886ab815494e9f717f8d708d92::getInitializer($loader));
         } else {
