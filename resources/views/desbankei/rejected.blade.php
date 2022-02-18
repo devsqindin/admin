d3b2cfdddc6ced4825711b58c12d59c03df8b5eb
Alyson Rodrigues <alyson.rosa@qindin.com.br>
2022-02-18 19:47:44 -0300

    Changing rejected blade email text

--- resources/views/desbankei/rejected.blade.php
+++ resources/views/desbankei/rejected.blade.php
@@ -9,7 +9,7 @@ Olá, {{$user->nome_completo}}!
   <td align="center" style="padding:0px 0px 30px 0px;font-family:'Source Sans Pro','Helvetica Neue',Calibre,Helvetica,Arial,sans-serif;font-weight:400;font-size:16px;line-height:23px;color:#000;text-align:left; padding: 0 5%;">
     <p><b>Obrigado por seu interesse no Qindin!</b></p>
     <p>Fizemos uma análise interna sobre o seu pedido de crédito.</p>
-    <p>Infelizmente, não conseguimos te conceder um limite de crédito nesse momento. Mas não se preocupe, você poderá tentar novamente daqui <b>3 meses</b>.</p>
+    <p>Infelizmente, não conseguimos te conceder um limite de crédito nesse momento. Mas não se preocupe, você poderá tentar novamente daqui <b>3 meses</b> ou sacar o seu saldo FGTS no <a href="https://app.qindin.com.br">Qindin FGTS</a>.</p>
 
     @if($user->motivo)
     <h5 style="font-weight:bold;font-size:16px;line-height:23px;color:#000;">Informações do Motivo</h5>
--- resources/views/desbankei/unregister.blade.php
+++ resources/views/desbankei/unregister.blade.php
@@ -8,7 +8,7 @@ Olá, {{$user->nome_completo}}!
 <tr>
   <td align="center" style="padding:0px 0px 30px 0px;font-family:'Source Sans Pro','Helvetica Neue',Calibre,Helvetica,Arial,sans-serif;font-weight:400;font-size:16px;line-height:23px;color:#000;text-align:left; padding: 0 5%;">
     <p><b>Seu cadastro foi bloqueado por irregularidades.</p>
-    <p>Por favor entre em <b>contato com o nosso time respondendo este e-mail, pelo <a href="mailto:contato@qindin.com.br">contato@qindin.com.br</a></b>. Estamos aqui por você! 😉</p>
+    <p>Por favor entre em <b>contato com o nosso time respondendo este e-mail, pelo <a href="mailto:contato@qindin.com.br">contato@qindin.com.br</a></b>.</p>
   </td>
 </tr>
 @endslot
